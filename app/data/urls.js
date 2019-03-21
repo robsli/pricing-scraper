5cd27738d16727e2795f4857f4621bef735df423
robsli <robbieli121.zy@gmail.com>
2019-03-20 21:55:06 -0400

    (adds) initial list of product urls

---
+++ app/data/urls.js
@@ -0,0 +1,38 @@
+const productLinks = {
+  bookshelves: [
+    'https://www.ikea.com/us/en/catalog/products/00263850/#/40263848',
+    'https://www.target.com/p/decorative-bookshelf-71-black-walnut-acme-furniture-174/-/A-52592810',
+    'https://www.target.com/p/72-carson-5-shelf-bookcase-threshold-153/-/A-14550115?preselect=11111129#lnk=sametab',
+    'https://www.target.com/p/72-carson-5-shelf-bookcase-with-doors-threshold-153/-/A-15007477?preselect=17315138#lnk=sametab',
+    'https://www.target.com/p/highpoint-contoured-71-bookcase-black-homes-inside-out/-/A-13941976',
+    'https://www.target.com/p/6-shelf-hutch-bookcase-82-black-walnut-closetmaid/-/A-52388070',
+    'https://www.wayfair.com/furniture/pdp/three-posts-lamantia-standard-bookcase-thre4526.html',
+    'https://www.wayfair.com/furniture/pdp/mercury-row-independence-standard-bookcase-w001387864.html?piid=631000083',
+    'https://www.wayfair.com/furniture/pdp/three-posts-abigail-standard-bookcase-thre4043.html?piid=17243528',
+    'https://www.wayfair.com/furniture/pdp/south-shore-axess-standard-bookcase-w000992118.html?piid=121838222',
+    'https://www.wayfair.com/furniture/pdp/greyleigh-macon-etagere-bookcase-gryl4640.html?piid=33392100',
+    'https://www.wayfair.com/furniture/pdp/williston-forge-swindell-standard-bookcase-w001108697.html?piid=1233339316',
+    'https://www.wayfair.com/furniture/pdp/trent-austin-design-cecelia-etagere-bookcase-trnt3743.html?piid=24761514',
+    'https://www.wayfair.com/furniture/pdp/ermont-etagere-bookcase-lrfy1920.html',
+    'https://www.wayfair.com/furniture/pdp/williston-forge-cieslak-etagere-bookcase-wlfr7782.html',
+    'https://www.wayfair.com/furniture/pdp/williston-forge-parkins-4-tier-portable-etagere-bookcase-w001416225.html',
+    'https://www.wayfair.com/furniture/pdp/williston-forge-bowman-68-multi-level-etagere-bookcase-wlk1928.html'
+  ],
+  coffeeTables: [
+
+  ],
+  nightstands: [
+
+  ],
+  sideTables: [
+    'https://www.wayfair.com/commercial/pdp/kate-and-laurel-usman-3-tiered-storage-cart-with-wheels-ktel1654.html',
+    'https://www.wayfair.com/commercial/pdp/mind-reader-charm-3-tier-wood-and-metal-utility-cart-mndr1129.html',
+    'https://www.wayfair.com/furniture/pdp/andover-mills-haines-flip-top-end-table-w000287535.html',
+    'https://www.wayfair.com/furniture/pdp/zipcode-design-annie-end-table-set-w000335923.html',
+    'https://www.wayfair.com/furniture/pdp/williston-forge-childers-2-tier-end-table-with-storage-cswy1536.html',
+    'https://www.wayfair.com/storage-organization/pdp/rebrilliant-10-drawer-storage-chest-fzv1180.html'
+  ]
+}
+
+
+module.exports = productLinks
